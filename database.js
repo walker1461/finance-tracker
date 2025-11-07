530d1fab043d7318341be952302d130d5385c69c
walker immel <walker1461@gmail.com>
2025-11-07 00:47:15 -0500

    implemented basic functionality for database.js

--- database.js
+++ database.js
@@ -1,1 +1,33 @@
 import Database from "better-sqlite3";
+
+const db = new Database("finance.db");
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS transactions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date TEXT NOT NULL,
+    description TEXT NOT NULL,
+    category TEXT NOT NULL,
+    amount REAL NOT NULL,
+    type TEXT NOT NULL
+    )
+`);
+
+export function addTransaction({ date, description, category, amount, type }) {
+  const statement = db.prepare(`
+    INSERT INTO transactions (date, description, category, amount, type)
+    VALUES (?, ?, ?, ?, ?)
+    `);
+  const info = statement.run(date, description, category, amount, type);
+  return info.lastInsertRowid;
+}
+
+export function getAllTransactions() {
+  const statement = db.prepare("SELECT * FROM transactions ORDER BY date DESC");
+  return statement.all();
+}
+
+export function deleteTransaction(id) {
+  const statement = db.prepare("DELETE FROM transactions WHERE id = ?");
+  return statement.run(id);
+}
